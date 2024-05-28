033e7691eb904e8d036aff0da6f34a2cd9cf8655
Nnamdi <koodu-platform@protonmail.com>
2024-05-28 11:45:24 +0100

    a

--- main.c
+++ main.c
@@ -13,13 +13,13 @@ const int SCREEN_HEIGHT = 480;
 int main() {
     SDL_Window* window = NULL;
     SDL_Surface* screenSurface = NULL;
-    
+
     // Initialize SDL
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
         return -1;
     }
-
+    
     // Create window
     window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
     if (window == NULL) {
